75f3e9cc00506939e7fdb11469bb48c19008724f
Tmaster <54488066+tmaster-terrarian@users.noreply.github.com>
2022-04-17 17:57:05 -0700

    fasfwafdsad

---
+++ Assets/Scripts/AdrenalineSystem.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Internal;
+
+public class AdrenalineSystem : MonoBehaviour
+{
+    Slider adrenalineBar = null;
+    GameObject adrenalineAlertText = null;
+    Volume adrenalinePPVolume = null;
+    bool isDraining = false;
+    [SerializeField] float adrenaline = 0;
+    [SerializeField] float adrenalineMax = 100;
+    [SerializeField] float adrenalineDrainRate = 3;
+    [SerializeField] float transitionInSpeed = 1;
+    [SerializeField] float transitionOutSpeed = 1;
+
+    [SerializeField] float debugPassiveGainRate = 0;
+
+    void Start()
+    {
+        adrenalineBar = GameObject.FindGameObjectWithTag("AdrenalineBar").GetComponent<Slider>();
+        adrenalineAlertText = GameObject.FindGameObjectWithTag("AdrenalineAlertText");
+        adrenalinePPVolume = GameObject.FindGameObjectWithTag("AdrenalinePPVolume").GetComponent<Volume>();
+        adrenalineAlertText.SetActive(false);
+        adrenalinePPVolume.weight = 0;
+        adrenalineBar.minValue = 0;
+        adrenalineBar.maxValue = adrenalineMax;
+    }
+
+    void Update()
+    {
+        adrenalineBar.value = adrenaline;
+        if(isDraining == false)
+        {
+            adrenaline += debugPassiveGainRate * 0.01f;
+            adrenalinePPVolume.weight = Mathf.Lerp(adrenalinePPVolume.weight, 0, transitionOutSpeed * Time.deltaTime);
+            adrenalinePPVolume.weight = Mathf.Clamp(adrenalinePPVolume.weight, 0.0001f, 0.9999f);
+        }
+        adrenaline = Mathf.Clamp(adrenaline, adrenalineBar.minValue, adrenalineBar.maxValue);
+
+        if(adrenaline == adrenalineMax)
+        {
+            adrenalineAlertText.SetActive(true);
+
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                adrenalinePPVolume.weight = 0;
+                isDraining = true;
+            }
+        }
+        else
+        {
+            adrenalineAlertText.SetActive(false);
+        }
+
+        DrainAdrenaline();
+        Debug();
+    }
+
+    void Debug()
+    {
+        if(Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            Add(10);
+        }
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Add(-10);
+        }
+    }
+
+    void DrainAdrenaline()
+    {
+        if(isDraining == true)
+        {
+            adrenalinePPVolume.weight = Mathf.Lerp(adrenalinePPVolume.weight, 1, transitionInSpeed * Time.deltaTime);
+            adrenalinePPVolume.weight = Mathf.Clamp(adrenalinePPVolume.weight, 0.0001f, 0.9999f);
+            adrenaline -= adrenalineBar.maxValue * 0.0001f * adrenalineDrainRate;
+
+            if(adrenaline <= 0)
+            {
+                adrenaline = 0;
+                isDraining = false;
+            }
+        }
+    }
+
+    ///<summary>Add (or subtract) a value from the player's adrenaline.</summary>
+    public void Add(float value)
+    {
+        adrenaline += value;
+    }
+}
